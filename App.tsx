13acaa7062fa98487c9296c5474e1c69082ee155
gccano <gccano@viptech.com.br>
2023-06-09 20:03:31 -0300

    feat(Button): created custom button component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,36 @@
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import {Button} from './src/components/Button/Button';
 import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge">headingLarge</Text>
-      <Text preset="headingMedium">headingMedium</Text>
-      <Text preset="headingSmall">headingSmall</Text>
-      <Text preset="paragraphMedium">paragraphMedium</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" bold italic disabled>
+            Nubble
+          </Text>
+          <Button title="Primary" marginBottom="s12" disabled />
+          <Button loading title="Loading" marginBottom="s12" disabled />
+
+          <Button
+            title="Outlined"
+            preset="outline"
+            marginBottom="s12"
+            disabled
+          />
+          <Button
+            disabled
+            loading
+            title="Outlined"
+            preset="outline"
+            marginBottom="s12"
+          />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
