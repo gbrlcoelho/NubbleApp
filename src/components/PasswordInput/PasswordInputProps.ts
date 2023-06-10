fd6e6dcfb5aeabcdda667e7ff327f40148780b6d
gbrlcoelho <coelhogabriel67@gmail.com>
2023-06-10 20:28:32 -0300

    feat(components): created password input component

---
+++ src/components/PasswordInput/PasswordInputProps.ts
@@ -0,0 +1,3 @@
+import {TextInputProps} from '@components/TextInput/TextInputProps';
+
+export type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ export {Box, TouchableOpacityBox} from './Box/Box';
 export type {BoxProps, TouchableOpacityBoxProps} from './Box/Box';
 export {Button} from './Button/Button';
 export {Icon} from './Icon/Icon';
+export {PasswordInput} from './PasswordInput/PasswordInput';
+export {Screen} from './Screen/Screen';
 export {Text} from './Text/Text';
 export {TextInput} from './TextInput/TextInput';
