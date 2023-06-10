5abd95c54526b83cd1eee2eba5119147292f4e6a
gbrlcoelho <coelhogabriel67@gmail.com>
2023-06-10 12:13:50 -0300

    feat(icons): added all project icons

---
+++ src/assets/icons/ChevronRightIcon.tsx
@@ -0,0 +1,17 @@
+import {IconBase} from '@components/Icon/IconProps';
+import React from 'react';
+import {Path, Svg} from 'react-native-svg';
+
+export const ChevronRightIcon = ({size = 20, color = 'black'}: IconBase) => {
+  return (
+    <Svg width={size} height={size} viewBox="0 0 20 20" fill="none">
+      <Path
+        d="M7 4L14 10L7 16"
+        stroke={color}
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
