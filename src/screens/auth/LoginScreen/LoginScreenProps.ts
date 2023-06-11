5adb892e77611d140bf24a069b2e78578b050e3f
gbrlcoelho <coelhogabriel67@gmail.com>
2023-06-11 19:41:20 -0300

    feat(screens): added navigation to login and sign up screen

---
+++ src/screens/auth/LoginScreen/LoginScreenProps.ts
@@ -0,0 +1,7 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '@routes';
+
+export type LoginScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'LoginScreen'
+>;
