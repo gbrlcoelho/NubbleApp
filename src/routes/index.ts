296e69807607861f37184d363d3d687a7519578a
gbrlcoelho <coelhogabriel67@gmail.com>
2023-06-21 21:02:47 -0300

    feat(routes): created app stack routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,6 @@
+export {AppStack} from './AppStack';
+export type {AppStackParamList} from './AppStack';
+export {AuthStack} from './AuthStack';
+export type {AuthStackParamList} from './AuthStack';
 export {Router} from './Routes';
-export type {RootStackParamList} from './Routes';
+export type {AppScreenProps, AuthScreenProps} from './navigationType';
