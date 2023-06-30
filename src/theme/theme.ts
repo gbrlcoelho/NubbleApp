457fe6f52b6ec7451be4009d19e5dda6aba6172b
gbrlcoelho <coelhogabriel67@gmail.com>
2023-06-29 22:42:55 -0300

    feat(theme): added icon marked color to the theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -37,6 +37,8 @@ export const theme = createTheme({
 
     success: palette.greenSuccess,
     successLight: palette.greenSuccessLight,
+
+    marked: palette.carrotSecondary,
   },
   spacing: {
     s4: 4,
