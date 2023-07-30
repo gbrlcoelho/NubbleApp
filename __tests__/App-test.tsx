73adf6cd7f366276efcdd34bae3d17e367b85630
gbrlcoelho <coelhogabriel67@gmail.com>
2023-07-30 19:38:53 -0300

    feat(root): add new script and run tsc --noEmit on husky pre-commit hook

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,12 +2,12 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
+import 'react-native';
 
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+import {App} from '../App';
 
 // Note: test renderer must be required after react-native.
 
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,4 +3,5 @@ export type {Post} from './Post/postTypes';
 export {usePostList} from './Post/useCases/usePostList';
 export {postCommentService} from './PostComment/postCommentService';
 export type {PostComment} from './PostComment/postCommentTypes';
+export {usePostCommentList} from './PostComment/useCases/usePostCommentList';
 export {usePaginatedList} from './hooks/usePaginatedList';
