e543c2b03cc237b7be8566fd46257d8c2256b2d1
gbrlcoelho <coelhogabriel67@gmail.com>
2023-07-30 14:16:05 -0300

    feat(domain): created use paginated list custom hook

---
+++ src/domain/hooks/usePaginatedList.ts
@@ -0,0 +1,65 @@
+import {useEffect, useState} from 'react';
+
+import {Page} from '@types';
+
+export const usePaginatedList = <T>(
+  getList: (page: number) => Promise<Page<T>>,
+) => {
+  const [list, setList] = useState<T[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const fetchInitialData = async () => {
+    try {
+      setError(false);
+      setLoading(true);
+      const {data, meta} = await getList(1);
+      setList(data);
+      if (meta.hasNextPage) {
+        setPage(2);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchNextPage = async () => {
+    if (loading || !hasNextPage) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const {data, meta} = await getList(page);
+      setList(prev => [...prev, ...data]);
+      if (meta.hasNextPage) {
+        setPage(prev => prev + 1);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    list,
+    error,
+    loading,
+    refresh: fetchInitialData,
+    fetchNextPage,
+  };
+};
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,6 @@
 export {postService} from './Post/postService';
 export type {Post} from './Post/postTypes';
 export {usePostList} from './Post/useCases/usePostList';
+export {postCommentService} from './PostComment/postCommentService';
+export type {PostComment} from './PostComment/postCommentTypes';
+export {usePaginatedList} from './hooks/usePaginatedList';
