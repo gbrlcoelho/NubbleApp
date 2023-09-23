1cdbd5ff4afb24e2dc178d2ba6c52bf88470dce4
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-23 14:46:43 -0300

    feat(routes): added profile screen to app routes

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -4,12 +4,13 @@ import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {AppTabBottomTabParamList, AppTabNavigator} from '@routes';
-import {PostCommentScreen, SettingsScreen} from '@screens';
+import {PostCommentScreen, ProfileScreen, SettingsScreen} from '@screens';
 
 export type AppStackParamList = {
   AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
   SettingsScreen: undefined;
   PostCommentScreen: {postId: number; postAuthorId: number};
+  ProfileScreen: {userId: number};
 };
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
@@ -25,6 +26,7 @@ export const AppStack = () => {
       <Stack.Screen name="AppTabNavigator" component={AppTabNavigator} />
       <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
       <Stack.Screen name="PostCommentScreen" component={PostCommentScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
