1f9bfd604acb042984e2fd9dfd031748004ee724
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-23 15:05:07 -0300

    chore(app): added react query and query client provider

--- App.tsx
+++ App.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 
 // import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
 import {Router} from '@routes';
 import {theme} from '@theme';
 
+const queryClient = new QueryClient();
+
 export const App = () => {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        {/* Only use ToastProvider if it is using Context implementation.
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          {/* Only use ToastProvider if it is using Context implementation.
           Zustand implementation doesn't need a provider */}
-        {/* <ToastProvider> */}
-        <Router />
-        <Toast />
-        {/* </ToastProvider> */}
-      </ThemeProvider>
-    </SafeAreaProvider>
+          {/* <ToastProvider> */}
+          <Router />
+          <Toast />
+          {/* </ToastProvider> */}
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 };
