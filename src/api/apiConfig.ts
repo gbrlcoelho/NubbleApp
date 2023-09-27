226d1d827c09054345c38f15ed35d9038951e303
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-27 20:36:54 -0300

    refactor(api): remove hard coded token

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,9 +1,6 @@
-import {REACT_APP_API_TOKEN, REACT_APP_BASE_URL} from '@env';
+import {REACT_APP_BASE_URL} from '@env';
 import axios from 'axios';
 
 export const api = axios.create({
   baseURL: REACT_APP_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${REACT_APP_API_TOKEN}`,
-  },
 });
