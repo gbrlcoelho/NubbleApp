509067ddef5df2444598e4524a35161b7f559687
gbrlcoelho <coelhogabriel67@gmail.com>
2023-10-09 22:01:34 -0300

    feat: implements auth credentials provider

--- App.tsx
+++ App.tsx
@@ -7,23 +7,26 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
 import {Router} from '@routes';
+import {AuthCredentialsProvider} from '@services';
 import {theme} from '@theme';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
-          {/* Only use ToastProvider if it is using Context implementation.
+    <AuthCredentialsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={theme}>
+            {/* Only use ToastProvider if it is using Context implementation.
           Zustand implementation doesn't need a provider */}
-          {/* <ToastProvider> */}
-          <Router />
-          <Toast />
-          {/* </ToastProvider> */}
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+            {/* <ToastProvider> */}
+            <Router />
+            <Toast />
+            {/* </ToastProvider> */}
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </AuthCredentialsProvider>
   );
 };
