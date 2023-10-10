0728f5e132d7ff8937c9b7435d91110c61ee76cf
gbrlcoelho <coelhogabriel67@gmail.com>
2023-10-09 22:00:33 -0300

    feat(routes): show loader when getting auth credentials from storage

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 
+import {ActivityIndicator, Box} from '@components';
 import {AppStack, AuthStack} from '@routes';
 import {useAuthCredentials} from '@services';
 
 export const Router = () => {
-  const {authCredentials} = useAuthCredentials();
+  const {authCredentials, isLoading} = useAuthCredentials();
+
+  if (isLoading) {
+    return (
+      <Box
+        flex={1}
+        backgroundColor="background"
+        justifyContent="center"
+        alignItems="center">
+        <ActivityIndicator size="large" />
+      </Box>
+    );
+  }
 
   return (
     <NavigationContainer>
