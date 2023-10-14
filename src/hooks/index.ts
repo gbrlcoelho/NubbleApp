27c4ff6bc850d18a71242e06b440e7d0df28fa9b
gbrlcoelho <coelhogabriel67@gmail.com>
2023-10-14 10:34:00 -0300

    refactor(hooks): default value to delay param

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export {useAppSafeArea} from './useAppSafeArea';
 export {useAppTheme} from './useAppTheme';
+export {useDebounce} from './useDebounce';
 export {useResetNavigationSuccess} from './useResetNavigationSuccess';
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -3,15 +3,15 @@ import {useEffect, useState} from 'react';
 /**
  * @description https://usehooks-ts.com/react-hook/use-debounce
  * @param value to be debounced
- * @param delay in milliseconds
+ * @param delay in milliseconds `default: 500ms`
  * @returns debounced value
  */
 
-export const useDebounce = <T>(value: T, delay?: number) => {
+export const useDebounce = <T>(value: T, delay = 500): T => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => clearTimeout(timer);
   }, [value, delay]);
