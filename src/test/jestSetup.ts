5de22bb12959c8a4c48de5cf8a26a19fe014f2e7
gbrlcoelho <coelhogabriel67@gmail.com>
2023-10-31 22:11:31 -0300

    test: created jest setup and added navigation container to all providers

---
+++ src/test/jestSetup.ts
@@ -0,0 +1,9 @@
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,12 +1,15 @@
 import React, {FC, ReactElement, ReactNode} from 'react';
 
+import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
 import {RenderOptions, render} from '@testing-library/react-native';
 
 import {theme} from '@theme';
 
 const AllTheProviders: FC<{children: ReactNode}> = ({children}) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <NavigationContainer>{children}</NavigationContainer>
+  </ThemeProvider>
 );
 
 const customRender = (
