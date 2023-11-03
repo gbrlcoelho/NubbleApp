2a1f0111071c91952cc2559e1348fadfb4449ea5
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-02 21:52:53 -0300

    test: added query client provider to test-utils

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,14 +2,36 @@ import React, {FC, ReactElement, ReactNode} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RenderOptions, render} from '@testing-library/react-native';
 
 import {theme} from '@theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // @ts-ignore
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+});
+
 export const AllTheProviders: FC<{children: ReactNode}> = ({children}) => (
-  <ThemeProvider theme={theme}>
-    <NavigationContainer>{children}</NavigationContainer>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>{children}</NavigationContainer>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
 
 const customRender = (
