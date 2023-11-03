282f6179805c5272be80723ec6de025040d30510
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-02 23:12:12 -0300

    test(domain): created tests for useAuthSignIn hook

---
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -0,0 +1,57 @@
+import {AllTheProviders, renderHook, waitFor} from 'test-utils';
+
+import {authService, useAuthSignIn} from '@domain';
+
+import {mockedAuthCredentials} from './mockedData/mocks';
+
+const mockedSaveCredentials = jest.fn();
+
+jest.mock('@services', () => {
+  const originalModule = jest.requireActual('@services');
+  return {
+    ...originalModule,
+    useAuthCredentials: () => ({
+      saveCredentials: mockedSaveCredentials,
+    }),
+  };
+});
+
+describe('useAuthSignIn', () => {
+  it('should save credentials if the sign in is successful', async () => {
+    jest
+      .spyOn(authService, 'signIn')
+      .mockResolvedValueOnce(mockedAuthCredentials);
+
+    const mockedOnSuccess = jest.fn();
+    const {result} = renderHook(
+      () => useAuthSignIn({onSuccess: mockedOnSuccess}),
+      {
+        wrapper: AllTheProviders,
+      },
+    );
+
+    result.current.signIn({email: 'gbrlcoelho@coffstack.com', password: '123'});
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(mockedSaveCredentials).toHaveBeenCalledWith(mockedAuthCredentials);
+    expect(mockedOnSuccess).toHaveBeenCalledWith(mockedAuthCredentials);
+  });
+
+  it('should call the onError callback if the sign in fails', async () => {
+    jest
+      .spyOn(authService, 'signIn')
+      .mockRejectedValue(new Error('invalid user'));
+
+    const mockedOnError = jest.fn();
+    const {result} = renderHook(() => useAuthSignIn({onError: mockedOnError}), {
+      wrapper: AllTheProviders,
+    });
+
+    result.current.signIn({email: 'gbrlcoelho@coffstack.com', password: '123'});
+
+    await waitFor(() => expect(result.current.isError).toBe(true));
+
+    expect(mockedOnError).toHaveBeenCalledWith('invalid user');
+  });
+});
