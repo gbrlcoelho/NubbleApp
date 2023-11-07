1d07ffdc6940f8962d8c4b58ff8945ca84cbda7d
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-06 22:06:37 -0300

    chore: added ts-node, created jest config and mock react-native-safe-area-context

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'react-native',
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
+  modulePathIgnorePatterns: ['.*/mockedData/.*'],
+  collectCoverageFrom: ['src/{components,utils,hooks,domain}/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
+  ],
+};
+
+export default config;
--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -1,5 +1,13 @@
 export {};
 
+// @ts-ignore
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+
+jest.mock('react-native-safe-area-context', () => ({
+  ...mockSafeAreaContext,
+  useSafeAreaInsets: jest.fn(mockSafeAreaContext.useAppSafeAreaInsets),
+}));
+
 jest.mock('@react-navigation/native', () => {
   const originalModule = jest.requireActual('@react-navigation/native');
   return {
