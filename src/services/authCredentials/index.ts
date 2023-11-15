9b01cebf5b5364412bc6ef43fdb369ecbefcbfef
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-15 00:34:13 -0300

    feat(services): export auth credentials storage

--- src/services/authCredentials/index.ts
+++ src/services/authCredentials/index.ts
@@ -1,3 +1,4 @@
 export {AuthCredentialsProvider} from './Providers/AuthCredentialsProvider';
+export {authCredentialsStorage} from './authCredentialsStorage';
 export * from './authCredentialsTypes';
 export * from './useAuthCredentials';
