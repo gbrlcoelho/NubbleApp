d89d88f68583d9c971816ba934fc9198feef44d3
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-15 00:36:22 -0300

    test(screens): integration tests on post comment screen

---
+++ src/screens/app/PostCommentScreen/__tests__/integration/PostCommentScreen.test.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import {Alert, AlertButton} from 'react-native';
+
+import {act} from 'react-test-renderer';
+import {
+  fireEvent,
+  renderScreen,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from 'test-utils';
+
+import {PostCommentScreen} from '@screens';
+import {authCredentialsStorage} from '@services';
+import {mockedPostComment, resetInMemoryResponse, server} from '@test';
+
+beforeAll(() => {
+  server.listen();
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  resetInMemoryResponse();
+});
+
+afterAll(() => {
+  server.close();
+  jest.resetAllMocks();
+  jest.useRealTimers();
+});
+
+describe('Integration: PostCommentScreen', () => {
+  it('should automatically update the list when adding a new comment', async () => {
+    renderScreen(
+      <PostCommentScreen
+        navigation={{} as any}
+        route={{
+          name: 'PostCommentScreen',
+          key: 'PostCommentScreen',
+          params: {
+            postId: 1,
+            postAuthorId: 1,
+          },
+        }}
+      />,
+    );
+
+    const comment = await screen.findByText(/comentário aleatório/i);
+
+    expect(comment).toBeTruthy();
+
+    const inputText = screen.getByPlaceholderText(/Adicione um comentário/i);
+
+    fireEvent.changeText(inputText, 'Novo comentário');
+
+    fireEvent.press(screen.getByText(/enviar/i));
+
+    const newComment = await screen.findByText(/novo comentário/i);
+    expect(newComment).toBeTruthy();
+
+    const comments = await screen.findAllByTestId('post-comment-id');
+
+    expect(comments).toHaveLength(3);
+  });
+
+  it('should automatically update the list when deleting a comment and show and show a toast message', async () => {
+    jest
+      .spyOn(authCredentialsStorage, 'get')
+      .mockResolvedValue(mockedPostComment.userAuthCredentials);
+
+    let mockedConfirm: AlertButton['onPress'];
+    const mockedAlert = jest
+      .spyOn(Alert, 'alert')
+      .mockImplementation((title, message, buttons) => {
+        if (buttons && buttons[0]) {
+          mockedConfirm = buttons[0].onPress;
+        }
+      });
+
+    renderScreen(
+      <PostCommentScreen
+        navigation={{} as any}
+        route={{
+          name: 'PostCommentScreen',
+          key: 'PostCommentScreen',
+          params: {
+            postId: 1,
+            postAuthorId: 1,
+          },
+        }}
+      />,
+    );
+
+    const comment = await screen.findByText(
+      mockedPostComment.userPostCommentAPI.message,
+      {exact: false},
+    );
+
+    expect(comment).toBeTruthy();
+
+    fireEvent(comment, 'longPress');
+
+    expect(mockedAlert).toHaveBeenCalled();
+
+    mockedConfirm && mockedConfirm();
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(mockedPostComment.userPostCommentAPI.message, {
+        exact: false,
+      }),
+    );
+
+    const comments = await screen.findAllByTestId('post-comment-id');
+
+    expect(comments.length).toBe(1);
+
+    await waitFor(() =>
+      expect(screen.getByTestId('toast-message')).toBeTruthy(),
+    );
+
+    act(() => jest.runAllTimers());
+
+    expect(screen.queryByTestId('toast-message')).toBeNull();
+  });
+});
