1002e58c2d8e485b0cd2a989bbad011ad1f1fcfd
gbrlcoelho <coelhogabriel67@gmail.com>
2023-12-21 23:22:39 -0300

    feat(domain): get user list

--- src/domain/User/userApi.ts
+++ src/domain/User/userApi.ts
@@ -1,14 +1,22 @@
-import {api} from '@api';
+import {PageAPI, api} from '@api';
 
 import {UserAPI} from './userTypes';
 
 const PATH = 'users';
 
-const getById = async (userId: string) => {
+const getById = async (userId: string): Promise<UserAPI> => {
   const response = await api.get<UserAPI>(`${PATH}/${userId}`);
+  return response.data;
+};
+
+const getList = async (search: string): Promise<PageAPI<UserAPI>> => {
+  const response = await api.get<PageAPI<UserAPI>>(`${PATH}`, {
+    params: {search},
+  });
   return response.data;
 };
 
 export const userApi = {
   getById,
+  getList,
 };
