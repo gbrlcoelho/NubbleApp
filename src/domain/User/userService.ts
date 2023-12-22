2fcedd3ed9c41483daef38e138b18f0c806fc895
gbrlcoelho <coelhogabriel67@gmail.com>
2023-12-21 23:44:11 -0300

    feat(domain): added user search use case

--- src/domain/User/userService.ts
+++ src/domain/User/userService.ts
@@ -8,12 +8,12 @@ const getById = async (id: number) => {
   return userAdapter.toUser(userAPI);
 };
 
-const getList = async (search: string) => {
+const searchUser = async (search: string) => {
   const userAPIPage = await userApi.getList(search);
   return apiAdapter.toPageModel(userAPIPage, userAdapter.toUser);
 };
 
 export const userService = {
   getById,
-  getList,
+  searchUser,
 };
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -8,16 +8,20 @@ export {
   useAuthEmailAvailable,
   useAuthUsernameAvailable,
 } from './Auth/useCases/useAuthValueIsAvailable';
+
 export {postService} from './Post/postService';
 export type {Post} from './Post/postTypes';
 export {usePostList} from './Post/useCases/usePostList';
+
 export {POST_COMMENT_PATH} from './PostComment/postCommentApi';
 export {postCommentService} from './PostComment/postCommentService';
 export type {PostComment, PostCommentAPI} from './PostComment/postCommentTypes';
 export {usePostCommentCreate} from './PostComment/useCases/usePostCommentCreate';
 export {usePostCommentList} from './PostComment/useCases/usePostCommentList';
 export {usePostCommentRemove} from './PostComment/useCases/usePostCommentRemove';
+
 export {useUserGetById} from './User/useCases/useUserGetById';
+export {useUserSearch} from './User/useCases/useUserSearch';
 export {userAdapter} from './User/userAdapter';
 export {userService} from './User/userService';
 export type {User, UserAPI} from './User/userTypes';
