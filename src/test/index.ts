284a80c9b3b47994f593b93d6100007cd70d4730
gbrlcoelho <coelhogabriel67@gmail.com>
2023-12-23 10:18:31 -0300

    feat(test-utils): config @react-navigation with jest and created mock utils

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,2 +1,3 @@
+export {mockUtils} from './mocks/mockUtils';
 export {mockedData as mockedPostComment} from './server/PostComment/mocks';
 export {resetInMemoryResponse, server} from './server/server';
---
+++ src/test/mocks/mockUtils.ts
@@ -0,0 +1,25 @@
+import {AuthCredentials, UserAPI, userAdapter} from '@domain';
+
+const userAPI: UserAPI = {
+  id: 7,
+  first_name: 'Gabriel',
+  last_name: 'Coelho',
+  username: 'gbrlcoelho',
+  email: 'gbrlcoelho@coffstack.com',
+  profile_url:
+    'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/8-mateus.png',
+  is_online: false,
+  full_name: 'Gabriel Coelho',
+};
+
+export const userAuthCredentials: AuthCredentials = {
+  token: 'access-token',
+  tokenExpiresAt: '2030-10-07T12:08:50.433+00:00',
+  refreshToken: 'refresh-token',
+  user: userAdapter.toUser(userAPI),
+};
+
+export const mockUtils = {
+  userAPI,
+  userAuthCredentials,
+};
