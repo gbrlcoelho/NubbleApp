299f573ac98cbcee9c7d848ac0ec9a9af3198656
gbrlcoelho <coelhogabriel67@gmail.com>
2024-02-12 13:41:04 -0300

    feat(screens): publish post screen

--- src/screens/app/PublishPostScreen/PublishPostScreen.tsx
+++ src/screens/app/PublishPostScreen/PublishPostScreen.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import {Dimensions, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Dimensions, FlexStyle, Image} from 'react-native';
 
-import {Screen} from '@components';
+import {Button, Screen, Text, TextInput} from '@components';
 import {AppScreenProps} from '@routes';
 
-const IMAGE_WIDTH = Dimensions.get('screen').width;
+const IMAGE_WIDTH = Dimensions.get('screen').width / 2;
+const ALIGN_SELF: FlexStyle['alignSelf'] = 'center';
+const MARGIN_TOP = 20;
 
-export const PublishPostScreen = ({}: AppScreenProps<'PublishPostScreen'>) => {
+export const PublishPostScreen = ({
+  route,
+}: AppScreenProps<'PublishPostScreen'>) => {
+  const {imageUri} = route.params;
+  const [description, setDescription] = useState('');
+
   return (
     <Screen scrollable canGoBack title="Novo Post">
       <Image
-        source={{uri: 'https://via.placeholder.com/150'}}
-        style={{width: IMAGE_WIDTH, height: IMAGE_WIDTH}}
+        source={{uri: imageUri}}
+        style={{
+          width: IMAGE_WIDTH,
+          height: IMAGE_WIDTH,
+          alignSelf: ALIGN_SELF,
+          marginTop: MARGIN_TOP,
+        }}
       />
+      <Text preset="headingSmall" marginTop="s32" marginBottom="s10">
+        Escreva uma legenda
+      </Text>
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder="Digite aqui..."
+        containerProps={{borderWidth: 0}}
+      />
+      <Button title="Publicar post" marginTop="s56" />
     </Screen>
   );
 };
