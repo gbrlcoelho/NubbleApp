619799f012052dd945015d8393f810d61817c1a1
gbrlcoelho <coelhogabriel67@gmail.com>
2024-02-13 13:20:38 -0300

    refactor(scripts): refactored scripts for scaffold a new component and domain to make more DRY

--- scripts/domain.rb
+++ scripts/domain.rb
@@ -1,75 +1,83 @@
 def camel_case?(str)
-  return false if str !~ /^[A-Z][a-zA-Z]*$/
-  words = str.split(/(?=[A-Z])/)
-  words.all? { |word| word =~ /^[A-Z][a-z]*$/ }
+  str =~ /^[A-Z][a-zA-Z]*$/
 end
 
-print "Provide a name for your domain in CamelCase: "
-domain_name = gets.chomp
-domain_name_downcase = domain_name.sub(/^(.)/) { $1.downcase }
+def create_folder(path)
+  Dir.mkdir(path) unless Dir.exist?(path)
+end
 
-unless camel_case?(domain_name)
+def create_file(path, content)
+  File.open(path, "w") { |file| file.puts content }
+end
+
+def get_domain_name
+  print "Provide a name for your domain in CamelCase: "
+  gets.chomp
+end
+
+def validate_domain_name(name)
+  unless camel_case?(name)
     puts "Domain name must be in CamelCase. Please provide a valid name."
     exit
-end
+  end
 
-domain_folder = "src/domain/#{domain_name}"
-if Dir.exist?(domain_folder)
-    puts "The domain #{domain_name} already exists. Please provide a different name."
+  if Dir.exist?("src/domain/#{name}")
+    puts "The domain #{name} already exists. Please provide a different name."
     exit
+  end
 end
 
-Dir.mkdir(domain_folder)
+def create_domain_files(name, folder, name_downcase)
+  adapter_file_content = <<~HEREDOC
+  import {#{name}, #{name}API} from './#{name_downcase}Types';
 
-domain_adapter_file = "#{domain_folder}/#{domain_name_downcase}Adapter.ts"
-  File.open(domain_adapter_file, "w") do |file|
-      file.puts "import {#{domain_name}, #{domain_name}API} from './#{domain_name_downcase}Types';"
-      file.puts ""
-      file.puts "const to#{domain_name} = (#{domain_name_downcase}API: #{domain_name}API): #{domain_name} => {"
-      file.puts "  return {};"
-      file.puts "};"
-      file.puts ""
-      file.puts "export const #{domain_name}Adapter = {"
-      file.puts "  to#{domain_name},"
-      file.puts "};"
-          
-  end
-  
-domain_api_file = "#{domain_folder}/#{domain_name_downcase}Api.ts"
-  File.open(domain_api_file, "w") do |file|
-      file.puts "import {api} from '@api';"
-      file.puts ""
-      file.puts "export const #{domain_name_downcase}API = {};"
-  end
+  const to#{name} = (#{name_downcase}API: #{name}API): #{name} => {
+    return {};
+  };
 
-domain_service_file = "#{domain_folder}/#{domain_name_downcase}Service.ts"
-  File.open(domain_service_file, "w") do |file|
-      file.puts "import {#{domain_name}API} from './#{domain_name_downcase}Types';"
-      file.puts ""
-      file.puts "export const #{domain_name_downcase}Service = {};"
-  end
+  export const #{name}Adapter = {
+    to#{name},
+  };
+  HEREDOC
+  create_file("#{folder}/#{name_downcase}Adapter.ts", adapter_file_content)
 
-domain_types_file = "#{domain_folder}/#{domain_name_downcase}Types.ts"
-  File.open(domain_types_file, "w") do |file|
-      file.puts "export interface #{domain_name}API {}"
-      file.puts ""
-      file.puts "export interface #{domain_name} {}"
-  end
+  api_file_content = <<~HEREDOC
+  import {api} from '@api';
 
-domain_usecase_folder = "#{domain_folder}/useCases"
+  export const #{name_downcase}API = {};
+  HEREDOC
+  create_file("#{folder}/#{name_downcase}Api.ts", api_file_content)
 
-Dir.mkdir(domain_usecase_folder)
+  service_file_content = <<~HEREDOC
+  import {#{name}API} from './#{name_downcase}Types';
 
-domain_index_file = "src/domain/index.ts"
+  export const #{name_downcase}Service = {};
+  HEREDOC
+  create_file("#{folder}/#{name_downcase}Service.ts", service_file_content)
 
-content = [
-  "export {#{domain_name_downcase}Adapter} from './#{domain_name_downcase}Adapter';",
-  "export {#{domain_name_downcase}Api} from './#{domain_name_downcase}Api';",
-  "export {#{domain_name_downcase}Service} from './#{domain_name_downcase}Service';",
-  "export type {#{domain_name}, #{domain_name}API} from './#{domain_name_downcase}Types';"
-]
+  types_file_content = <<~HEREDOC
+  export interface #{name}API {}
 
-
-File.open(domain_index_file, "a") do |file| 
-  content.each { |line| file.puts(line) }
+  export interface #{name} {}
+  HEREDOC
+  create_file("#{folder}/#{name_downcase}Types.ts", types_file_content)
 end
+
+def create_domain
+  domain_name = get_domain_name
+  validate_domain_name(domain_name)
+
+  domain_name_downcase = domain_name.sub(/^(.)/) { $1.downcase }
+
+  domain_folder = "src/domain/#{domain_name}"
+  create_folder(domain_folder)
+
+  create_domain_files(domain_name, domain_folder, domain_name_downcase)
+
+  domain_usecase_folder = "#{domain_folder}/useCases"
+  create_folder(domain_usecase_folder)
+
+  puts "Domain #{domain_name} created successfully in #{domain_folder}."
+end
+
+create_domain
--- scripts/scaffold.rb
+++ scripts/scaffold.rb
@@ -1,160 +1,90 @@
 def camel_case?(str)
-    return false if str !~ /^[A-Z][a-zA-Z]*$/
-    words = str.split(/(?=[A-Z])/)
-    words.all? { |word| word =~ /^[A-Z][a-z]*$/ }
-  end
-  
+  str =~ /^[A-Z][a-zA-Z]*$/
+end
+
+def create_folder(path)
+  Dir.mkdir(path) unless Dir.exist?(path)
+end
+
+def create_file(path, content)
+  File.open(path, "w") { |file| file.puts content }
+end
+
+def get_component_name
   print "Provide a name for your component in CamelCase: "
-  component_name = gets.chomp
-  
-  unless camel_case?(component_name)
-      puts "Component name must be in CamelCase. Please provide a valid name."
-      exit
+  gets.chomp
+end
+
+def validate_component_name(name)
+  unless camel_case?(name)
+    puts "Component name must be in CamelCase. Please provide a valid name."
+    exit
   end
-  
+
+  if Dir.exist?("src/components/#{name}")
+    puts "The component #{name} already exists. Please provide a different name."
+    exit
+  end
+end
+
+def create_component_files(name, folder)
+  props_file_content = "export interface #{name}Props {}"
+  create_file("#{folder}/#{name}Props.ts", props_file_content)
+
+  component_file_content = <<-HEREDOC
+    import React from 'react';
+    import {Box} from '@components';
+    import {#{name}Props} from './#{name}Props';
+
+    export const #{name} = ({}: #{name}Props) => {
+      return (
+        <Box>
+          <></>
+        </Box>
+      );
+    };
+  HEREDOC
+  create_file("#{folder}/#{name}.tsx", component_file_content)
+end
+
+def create_test_files(name, folder)
+  test_file_content = <<-HEREDOC
+    import React from 'react';
+    import {render} from '@testing-library/react-native';
+    import {#{name}} from '../#{name}';
+
+    describe('#{name}', () => {
+      it('should render correctly', () => {
+        const {toJSON} = render(<#{name} />);
+        expect(toJSON()).toMatchSnapshot();
+      });
+    });
+  HEREDOC
+  create_file("#{folder}/#{name}.spec.tsx", test_file_content)
+end
+
+def add_to_index(name)
+  index_file_path = "src/components/index.ts"
+  File.open(index_file_path, "a") { |file| file.puts "export {#{name}} from './#{name}/#{name}';" }
+end
+
+def create_component
+  component_name = get_component_name
+  validate_component_name(component_name)
+
   component_folder = "src/components/#{component_name}"
-  if Dir.exist?(component_folder)
-      puts "The component #{component_name} already exists. Please provide a different name."
-      exit
-  end
-  
-  Dir.mkdir(component_folder)
-  
-  component_props_file = "#{component_folder}/#{component_name}Props.ts"
-  File.open(component_props_file, "w") do |file|
-    file.puts "export interface #{component_name}Props {}"
-  end
-  
-  component_file = "#{component_folder}/#{component_name}.tsx"
-  File.open(component_file, "w") do |file|
-    file.puts "import React from 'react';"
-    file.puts ""
-    file.puts "import {Box} from '@components';"
-    file.puts ""
-    file.puts "import {#{component_name}Props} from './#{component_name}Props';"
-    file.puts ""
-    file.puts "export const #{component_name} = ({}: #{component_name}Props) => {"
-    file.puts "  return ("
-    file.puts "    <Box>"
-    file.puts "      <></>"
-    file.puts "    </Box>"
-    file.puts "  );"
-    file.puts "};"
-  end
-  
+  create_folder(component_folder)
+
+  create_component_files(component_name, component_folder)
+
   component_test_folder = "#{component_folder}/__tests__"
-  Dir.mkdir(component_test_folder)
-  
-  component_test_file = "#{component_test_folder}/#{component_name}.spec.tsx"
-  
-  File.open(component_test_file, "w") do |file|
-    file.puts "import React from 'react';"
-    file.puts ""
-    file.puts "import {render} from '@testing-library/react-native';"
-    file.puts ""
-    file.puts "import {#{component_name}} from '../#{component_name}';"
-    file.puts ""
-    file.puts "describe('#{component_name}', () => {"
-    file.puts "  it('should render correctly', () => {"
-    file.puts "    const {toJSON} = render(<#{component_name} />);"
-    file.puts "    expect(toJSON()).toMatchSnapshot();"
-    file.puts "  });"
-    file.puts "});"
-  end
-  
+  create_folder(component_test_folder)
+
+  create_test_files(component_name, component_test_folder)
+
+  add_to_index(component_name)
+
   puts "Component #{component_name} created successfully in #{component_folder}."
-  
-  # FILEPATH: /Users/user/Study/Nubble/NubbleApp/scaffold.js
-  # const readline = require('readline');
-  # const fs = require('fs');
-  
-  # const rl = readline.createInterface({
-  #   input: process.stdin,
-  #   output: process.stdout,
-  # });
-  
-  # function camelCase(str) {
-  #   return (
-  #     /^[A-Z][a-zA-Z]*$/.test(str) &&
-  #     str.split(/(?=[A-Z])/).every(word => /^[A-Z][a-z]*$/.test(word))
-  #   );
-  # }
-  
-  # rl.question(
-  #   'Provide a name for your component in CamelCase: ',
-  #   function (component_name) {
-  #     if (!camelCase(component_name)) {
-  #       console.log(
-  #         'Component name must be in CamelCase. Please provide a valid name.',
-  #       );
-  #       rl.close();
-  #       return;
-  #     }
-  
-  #     const component_folder = `src/components/${component_name}`;
-  #     if (fs.existsSync(component_folder)) {
-  #       console.log(
-  #         `The component ${component_name} already exists. Please provide a different name.`,
-  #       );
-  #       rl.close();
-  #       return;
-  #     }
-  
-  #     fs.mkdirSync(component_folder);
-  
-  #     const component_props_file = `${component_folder}/${component_name}Props.ts`;
-  #     fs.writeFileSync(
-  #       component_props_file,
-  #       `export interface ${component_name}Props {}
-        
-  #       `,
-  #     );
-  
-  #     const component_file = `${component_folder}/${component_name}.tsx`;
-  #     fs.writeFileSync(
-  #       component_file,
-  #       `import React from 'react';
-    
-  # import {Box} from '@components';
-  
-  # import {${component_name}Props} from './${component_name}Props';
-  
-  # export const ${component_name} = ({}: ${component_name}Props) => {
-  #   return (
-  #     <Box>
-  #       <></>
-  #     </Box>
-  #   );
-  # };
-  
-  # `,
-  #     );
-  
-  #     const component_test_folder = `${component_folder}/__tests__`;
-  #     fs.mkdirSync(component_test_folder);
-  
-  #     const component_test_file = `${component_test_folder}/${component_name}.spec.tsx`;
-  #     fs.writeFileSync(
-  #       component_test_file,
-  #       `import {render} from '@testing-library/react-native';
-  
-  # import {${component_name}} from '../${component_name}';
-  
-  # describe('#{component_name}', () => {
-  #   it('should render correctly', () => {
-  #     const {toJSON} = render(<${component_name} />);
-  #     expect(toJSON()).toMatchSnapshot();
-  #   });
-  # });
-  
-  # `,
-  #     );
-  
-  #     console.log(
-  #       `Component ${component_name} created successfully in ${component_folder}.`,
-  #     );
-  #     rl.close();
-  #   },
-  # );
-  
-  
+end
+
+create_component
