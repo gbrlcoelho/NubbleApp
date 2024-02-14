11823e1617fe8cedcbe786ce50d7605164a75c24
gbrlcoelho <coelhogabriel67@gmail.com>
2024-02-14 11:54:19 -0300

    test(mocks): mock react-native-vision-camera

---
+++ __mocks__/react-native-vision-camera/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export const Camera = () => {
+  return <View />;
+};
+
+export const useCameraDevice = jest.fn();
+export const useCamearaFormat = jest.fn();
