494bfbb5139a82a387838135f4a0eafa7ac1badd
gbrlcoelho <coelhogabriel67@gmail.com>
2024-02-23 22:30:19 -0300

    test(mocks): mock expo-image-manipulator

--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -38,4 +38,8 @@ jest.mock('../services/permission/permissionService', () => ({
   },
 }));
 
+jest.mock('expo-image-manipulator', () => ({
+  manipulateAsync: jest.fn(),
+}));
+
 initializeStorage(inMemoryStorage);
