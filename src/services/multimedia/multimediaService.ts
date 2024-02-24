f5c6ff565a1be6793852e0f2d8548f08f2e94bbb
gbrlcoelho <coelhogabriel67@gmail.com>
2024-02-23 22:33:16 -0300

    feat(services): prepare image uri and for upload on multimedia services

--- src/services/multimedia/multimediaService.ts
+++ src/services/multimedia/multimediaService.ts
@@ -1,4 +1,7 @@
+import {Platform} from 'react-native';
+
 import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import {SaveFormat, manipulateAsync} from 'expo-image-manipulator';
 
 import {ImageForUpload, PhotoListPaginated} from './multimediaTypes';
 
@@ -13,16 +16,46 @@ const getPhotos = async (cursor?: string): Promise<PhotoListPaginated> => {
   };
 };
 
-const prepareImageForUpload = (imageUri: string): ImageForUpload => {
-  // TODO: Implement this function
+/**
+ * Prepare image uri for upload
+ * @param imageUri image path as provided by either camera or gallery modules
+ *
+ * @returns an imageUri ready to be used in the `Image` component and `FormData` requests
+ */
+const prepareImageUri = (imageUri: string): string => {
+  if (Platform.OS !== 'android') {
+    return imageUri;
+  }
+
+  if (imageUri.startsWith('file://')) {
+    return imageUri;
+  }
+
+  return `file://${imageUri}`;
+};
+
+/**
+ * Prepare image for upload
+ * @param imageUri image path
+ * @returns `ImageForUpload` - an object with props requested by a `FormData`.
+ */
+const prepareImageForUpload = async (
+  imageUri: string,
+): Promise<ImageForUpload> => {
+  const image = await manipulateAsync(prepareImageUri(imageUri), [], {
+    compress: 0.5,
+    format: SaveFormat.JPEG,
+  });
+
   return {
-    uri: imageUri,
-    name: 'name',
-    type: 'image/png',
+    uri: image.uri,
+    name: Date.now().toString(),
+    type: 'image/jpeg',
   };
 };
 
 export const multimediaService = {
   prepareImageForUpload,
+  prepareImageUri,
   getPhotos,
 };
