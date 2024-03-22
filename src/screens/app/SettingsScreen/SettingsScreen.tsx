b2256ca2de55491fef1ee60f055b873f72dd264c
gbrlcoelho <coelhogabriel67@gmail.com>
2024-03-21 21:06:50 -0300

    feat(screens): settings screens menu and menu item component

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import {FlatList, ListRenderItem} from 'react-native';
 
-import {Button, Screen} from '@components';
+import {Button, Screen, Separator} from '@components';
 import {useAuthSignOut} from '@domain';
 import {AppScreenProps} from '@routes';
 
-export const SettingsScreen = ({}: AppScreenProps<'SettingsScreen'>) => {
+import {MenuItem, type MenuItemProps} from './components';
+
+export const SettingsScreen = ({
+  navigation,
+}: AppScreenProps<'SettingsScreen'>) => {
   const {signOut, isLoading} = useAuthSignOut();
 
+  const items: MenuItemProps[] = [
+    {label: 'Termos de uso', onPress: () => {}},
+    {label: 'Política de privacidade', onPress: () => {}},
+    {
+      label: 'Modo Escuro',
+      onPress: () => navigation.navigate('DarkModeScreen'),
+    },
+  ];
+
+  const renderItem: ListRenderItem<MenuItemProps> = ({item}) => {
+    return <MenuItem {...item} />;
+  };
+
   return (
-    <Screen canGoBack title="Configurações">
-      <Button title="Sair da conta" onPress={signOut} loading={isLoading} />
+    <Screen canGoBack title="Configurações" flex={1}>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={item => item.label}
+        ItemSeparatorComponent={Separator}
+        ListFooterComponent={
+          <Button
+            title="Sair da conta"
+            onPress={signOut}
+            loading={isLoading}
+            marginTop="s48"
+          />
+        }
+        bounces={false}
+      />
     </Screen>
   );
 };
