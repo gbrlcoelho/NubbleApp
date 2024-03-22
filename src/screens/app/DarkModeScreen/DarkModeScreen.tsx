74a013b11222bec95461f1c31541372ec8d912f8
gbrlcoelho <coelhogabriel67@gmail.com>
2024-03-21 21:57:02 -0300

    feat(screens): dark mode screen with radio button selector

--- src/screens/app/DarkModeScreen/DarkModeScreen.tsx
+++ src/screens/app/DarkModeScreen/DarkModeScreen.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-import {Screen, Text} from '@components';
+import {RadioButtonSelector, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
+
+const items = [
+  {label: 'Ativado', onPress: () => {}, isSelected: false},
+  {label: 'Desativado', isSelected: true, onPress: () => {}},
+  {
+    label: 'Padrão do sistema',
+    description:
+      'A aparência será a mesma que você configurou no seu dispositivo',
+    onPress: () => {},
+    isSelected: false,
+  },
+];
 
 export const DarkModeScreen = ({}: AppScreenProps<'DarkModeScreen'>) => {
   return (
     <Screen canGoBack title="Modo escuro">
       <Text>DarkModeScreen</Text>
+      <RadioButtonSelector items={items} />
     </Screen>
   );
 };
