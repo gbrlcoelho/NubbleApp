a3f84d96d6237f0cbf36d01c0aba0f709f869960
gbrlcoelho <coelhogabriel67@gmail.com>
2024-03-23 11:07:18 -0300

    feat(theme): added dark theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,7 +5,7 @@ import {createTheme} from '@shopify/restyle';
 import {colors} from './colors';
 
 export const theme = createTheme({
-  colors: colors.darkTheme,
+  colors: colors.lightTheme,
   spacing: {
     s4: 4,
     s8: 8,
@@ -29,6 +29,11 @@ export const theme = createTheme({
   textVariants: {
     defaults: {},
   },
+});
+
+export const darkTheme = createTheme({
+  ...theme,
+  colors: colors.darkTheme,
 });
 
 export const $shadowProps: ViewStyle = {
