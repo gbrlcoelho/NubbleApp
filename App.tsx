3b2d23517f5807e86d47b7caac42bbd16003e078
gbrlcoelho <coelhogabriel67@gmail.com>
2024-03-23 11:17:48 -0300

    feat(app): check app color scheme so app can use dark or light mode

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
 // import {ToastProvider} from '@services';
+
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
+import {useAppColorScheme} from '@hooks';
 import {Router} from '@routes';
-import {theme} from '@theme';
+import {settingsService, useAppColor} from '@services';
+import {darkTheme, theme} from '@theme';
 
 import {AuthCredentialsProvider} from './src/services/authCredentials';
 import {MMKVStorage, initializeStorage} from './src/services/storage';
@@ -17,11 +19,19 @@ const queryClient = new QueryClient();
 initializeStorage(MMKVStorage);
 
 export const App = () => {
+  const appColor = useAppColor();
+
+  useAppColorScheme();
+
+  useEffect(() => {
+    settingsService.handleStatusBarStyle(appColor);
+  }, [appColor]);
+
   return (
     <AuthCredentialsProvider>
       <QueryClientProvider client={queryClient}>
         <SafeAreaProvider>
-          <ThemeProvider theme={theme}>
+          <ThemeProvider theme={appColor === 'dark' ? darkTheme : theme}>
             {/* Only use ToastProvider if it is using Context implementation.
           Zustand implementation doesn't need a provider */}
             {/* <ToastProvider> */}
