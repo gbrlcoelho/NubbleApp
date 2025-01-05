06f74756bfed821b7156ab703d2ff228838370a0
gbrlcoelho <coelhogabriel67@gmail.com>
2025-01-05 00:29:46 -0300

    feat: set background message handler

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
+import messaging from '@react-native-firebase/messaging';
 import {AppRegistry} from 'react-native';
 import {App} from './App';
 import {name as appName} from './app.json';
+
+messaging().setBackgroundMessageHandler(async () => {});
 
 AppRegistry.registerComponent(appName, () => App);
